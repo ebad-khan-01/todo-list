f21874032c7c870480b307bf43ddc7aaa13f0d2f
muhammadebad <ebadk739@gmail.com>
2024-11-29 06:54:17 +0500

    Updated the code with new features/bug fixes

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,67 +1,83 @@
 import React, { useState } from "react";
 import "./todo.css";
-import todoimg from "../images/todo.webp";
 
-function Todo() {
+const Todo = () => {
   const [inputData, setInputData] = useState('');
   const [items, setItems] = useState([]);
 
+  // Add an item to the list
   const addItem = () => {
-    if (inputData.trim() !== '') {
-      setItems([...items, inputData]);
+    if (inputData.trim()) {
+      setItems([...items, inputData.trim()]);
       setInputData('');
     }
   };
 
+  // Delete an item from the list
   const deleteItem = (indexToDelete) => {
-    // console.log(indexToDelete)
-    const updatedItems = items.filter((ele, index) => index !== indexToDelete);
-    setItems(updatedItems);
+    setItems((prevItems) => prevItems.filter((_, index) => index !== indexToDelete));
   };
-  const removeAll= ()=>{
-    setItems([])
-  }
+
+  // Remove all items from the list
+  const removeAll = () => {
+    setItems([]);
+  };
 
   return (
-    <>
-      <div className="main-div">
-        <div className="child-div">
-          <figure>
-            <img src={todoimg} alt="todo" />
-            <figcaption>Add Your List Here</figcaption>
-          </figure>
-          <div className="add-items">
-            <input
-              type="text"
-              placeholder="Add items"
-              value={inputData}
-              onChange={(e) => setInputData(e.target.value)}
-            />
-            <i
-              className="fa fa-plus add-btn"
-              title="add items"
-              onClick={addItem}
-            ></i>
-          </div>
-          <div className="show-items-container">
-              {items.map((elem, index) => (
-                <div className="each-item" key={index}>
-                  <h3>{elem}</h3>
-                  <i
-                    className="fa fa-trash add-btn"
-                    title="delete item"
-                    onClick={() => deleteItem(index)}
-                  ></i>
-                </div>
-              ))}
-          </div>
-          <div className="remove-btn">
-            <button onClick={()=> removeAll()}>Check list</button>
-          </div>
+    <div className="main-div">
+      <div className="child-div">
+        <figure>
+          <img src="./todo.webp" alt="Todo App" />
+          <figcaption>Add Your List Here</figcaption>
+        </figure>
+
+        {/* Add Items Section */}
+        <div className="add-items">
+          <input
+            type="text"
+            placeholder="Add items"
+            value={inputData}
+            onChange={(e) => setInputData(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' && addItem()} // Add item on pressing Enter
+          />
+          <button
+            className="add-btn"
+            title="Add Item"
+            onClick={addItem}
+          >
+            +
+          </button>
         </div>
+
+        {/* Show Items Section */}
+        <div className="show-items">
+          {items.length > 0 ? (
+            items.map((item, index) => (
+              <div className="each-item" key={index}>
+                <h3>{item}</h3>
+                <button
+                  className="delete-btn"
+                  title="Delete Item"
+                  onClick={() => deleteItem(index)}
+                >
+                  🗑
+                </button>
+              </div>
+            ))
+          ) : (
+            <p className="no-items">Your list is empty. Start adding items!</p>
+          )}
+        </div>
+
+        {/* Remove All Button */}
+        {items.length > 0 && (
+          <div className="remove-btn">
+            <button onClick={removeAll}>Clear List</button>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default Todo;
